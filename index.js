880fcd342bdcc033d462d47a7fa5423fface8181
atrn0 <14242642+aratasato@users.noreply.github.com>
2020-07-19 17:53:19 +0900

    trigger gesture

--- index.js
+++ index.js
@@ -1,5 +1,31 @@
 const Leap = require("leapjs");
 
-Leap.loop(function (frame) {
-  console.log(frame.hands.length);
-});
+// Leap.loop(function (frame) {
+//   console.log(frame.hands.length);
+// });
+
+const controller = new Leap.Controller();
+controller.connect();
+
+controller.on("gesture", onGesture);
+
+let lastTriggerTime = 0;
+
+function onGesture(gesture, frame) {
+  if (gesture.type === "swipe") {
+    const now = new Date();
+    if (lastTriggerTime + 500 < now.getTime()) {
+      if (gesture.direction[1] > 0.2) {
+        lastTriggerTime = now.getTime();
+        onTriggered(true);
+      } else if (gesture.direction[1] < -0.2) {
+        lastTriggerTime = now.getTime();
+        onTriggered(false);
+      }
+    }
+  }
+}
+
+function onTriggered(makeLightsOn) {
+  console.log(makeLightsOn ? "on" : "off");
+}
